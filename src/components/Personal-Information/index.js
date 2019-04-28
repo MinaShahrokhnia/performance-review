300d641e58b92a37e3ddf0baf9a4fdbf1bfed1ed
Mina <minashahrokhnia@Minas-MacBook-Air.local>
2019-04-28 08:18:18 +0430

    linitialize the state with personal information

--- src/components/Personal-Information/index.js
+++ src/components/Personal-Information/index.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 
 class PersonalInformation extends Component {
-    constructor(){
+    constructor() {
         super();
         this.state = {
-            reviewerName:''
+            reviewerName: '',
+            name: '',
+            date: ''
         }
     }
+
+    changeName = ( event ) => {
+        const value = event.target.value;
+        this.setState({ reviewerName: value });
+    }
+
     render() {
         return(
             <form>
-                <input type="text"/>
+                <input type="text" className='reviwer-name'
+                onChange={this.changeName}
+                value={this.state.reviewerName}/>
                 <input type="text"/>
                 <input type="date"/>
             </form>
--- src/components/Personal-Information/index.test.js
+++ src/components/Personal-Information/index.test.js
@@ -10,4 +10,21 @@ it('renders `personal-information` component', () => {
 
 it('initialize the `state` with empty reviewer name', () => {
     expect(informationPersonal.state().reviewerName).toEqual('');
+});
+
+it('initialize the `state` with empty reviewer name', () => {
+    expect(informationPersonal.state().reviewerName).toEqual('');
+});
+
+it('initialize the `state` with empty name', () => {
+    expect(informationPersonal.state().name).toEqual('');
+});
+
+it('initialize the `state` with empty date', () => {
+    expect(informationPersonal.state().date).toEqual('');
+});
+
+it('should accept `state` with onchange', () => {
+    PersonalInformation.find('.reviewer-name').simiulate('change');
+    expect(informationPersonal.state().reviewerName).toEqual('mina')
 })
